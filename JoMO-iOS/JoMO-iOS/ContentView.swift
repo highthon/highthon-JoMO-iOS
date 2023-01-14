e4b0ccd52d84952183f7d2a4ceca87d1422ba393
최정현 <choi.jeonghyun@mintech.kr>
2023-01-14 20:08:23 +0900

    [add]: xcodeproject 생성

---
+++ JoMO-iOS/JoMO-iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  JoMO-iOS
+//
+//  Created by 짜미 on 2023/01/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JoMO-iOS/JoMO-iOS/JoMO_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoMO_iOSApp.swift
+//  JoMO-iOS
+//
+//  Created by 짜미 on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct JoMO_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
