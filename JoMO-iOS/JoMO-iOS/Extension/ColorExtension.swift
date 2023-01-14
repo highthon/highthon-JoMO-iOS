b0d159d746a9d8087d49f94f1a9b09e8a89cc054
최정현 <yangpa043@gmail.com>
2023-01-14 22:47:53 +0900

    [style]: colorExtension 적용

--- JoMO-iOS/JoMO-iOS/Extension/ColorExtension.swift
+++ JoMO-iOS/JoMO-iOS/Extension/ColorExtension.swift
@@ -8,5 +8,12 @@
 import SwiftUI
 
 extension Color {
-//    static let mainGray = Color("mainGray")
+    static let mainYellow = Color("PrimaryYellow")
+    static let mainDarkblue = Color("SecondaryDarkblue")
+    static let mainSkyblue = Color("SecondarySkyblue")
+    static let gray1 = Color("Gray 1")
+    static let gray2 = Color("Gray 2")
+    static let gray3 = Color("Gray 3")
+    static let gray4 = Color("Gray 4")
+    static let gray5 = Color("Gray 5")
 }
