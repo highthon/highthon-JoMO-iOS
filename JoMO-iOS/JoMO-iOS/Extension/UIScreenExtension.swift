543971f5ae231b21b53701d985d774b55b572f23
최정현 <yangpa043@gmail.com>
2023-01-14 20:50:26 +0900

    [add]: UIScreenExtension 추기

---
+++ JoMO-iOS/JoMO-iOS/Extension/UIScreenExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UIScreenExtension.swift
+//  JoMO-iOS
+//
+//  Created by 짜미 on 2022/09/28.
+//
+
+import Foundation
+import SwiftUI
+
+extension UIScreen {
+  static let screenWidth = UIScreen.main.bounds.size.width
+  static let screenHeight = UIScreen.main.bounds.size.height
+  static let screenSize = UIScreen.main.bounds.size
+}
